4fb80ce00c239a014c57460b3c8ca06ad04d80b0
Shink <shink_7770@qq.com>
2021-06-18 23:42:20 +0800

    不再提供IE浏览器版本常量

--- constants.rb
+++ constants.rb
@@ -4,10 +4,4 @@ module SHINK_LIBRARY
   MainThread = Thread.current
   SuVersion = Sketchup.version.split('.').first.to_i
   DocumentsPath = ENV['HOME'].encode('UTF-8')
-  if IsWindows
-    require 'win32/registry'
-    ie_version1 = Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\Internet Explorer'){|reg| reg['svcVersion'].split('.').first.to_i} rescue nil
-    ie_version2 = Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Wow6432Node\Microsoft\Internet Explorer'){|reg| reg['svcVersion'].split('.').first.to_i} rescue nil
-    IEVersion = ie_version1 || ie_version2 || 0
-  end
 end
