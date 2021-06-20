c62760e0d786f63f8f832fb8e1948f2c598acc8d
Shink <shink_7770@qq.com>
2021-06-20 23:40:14 +0800

    使用Sketchup::require加载代码文件

--- su_db.rb
+++ su_db.rb
@@ -1,12 +1,12 @@
 module SHINK_LIBRARY
   module SuDb
-    require_relative('su_db/base_db')
-    require_relative('su_db/config')
-    require_relative('su_db/auto_save_db')
-    require_relative('su_db/hash_db')
-    require_relative('su_db/array_db')
-    require_relative('su_db/open_struct_array')
-    require_relative('su_db/table')
+    Sketchup::require 'su_db/base_db'
+    Sketchup::require 'su_db/config'
+    Sketchup::require 'su_db/auto_save_db'
+    Sketchup::require 'su_db/hash_db'
+    Sketchup::require 'su_db/array_db'
+    Sketchup::require 'su_db/open_struct_array'
+    Sketchup::require 'su_db/table'
     OSArray = OpenStructArray
   end
 end
